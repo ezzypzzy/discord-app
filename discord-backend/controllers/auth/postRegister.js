a40bb487a1e9b1ef9b1d1ad54166a06ba5ef37ac
Asutosh <ashutoshguleria9@gmail.com>
2023-05-23 07:00:33 +0530

    add registering functionality

--- discord-backend/controllers/auth/postRegister.js
+++ discord-backend/controllers/auth/postRegister.js
@@ -1,5 +1,44 @@
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+
 const postRegister = async (req, res) => {
-  res.send('register route');
+  try {
+    const { username, password, mail } = req.body;
+
+    // Check if user already exists 
+    const userExists = await User.exists({ mail: mail.toLowerCase() });
+
+    if (userExists) {
+      // 409 Conflict response status code indicates a request conflict with current state of the server
+      return res.status(409).json({ error: "E-mail already in use" });
+
+      // Encrypt the password
+      // 10 is the salt rounds value (default is 10)
+      const encryptedPassword = await bcrypt.hash(password, 10);
+
+      // Create user document and save it to the database
+      const user = await User.create({
+        username,
+        mail: mail.toLowerCase(),
+        password: encryptedPassword,
+      });
+    }
+    
+    // Create JWT token and return that to the client (saved at the client side)
+    // with this token, user will be able to access the protected routes
+    const token = "JWT TOKEN";
+
+    res.status(201).json({ 
+      userDetails: {
+        mail: user.mail,
+        token,
+        username
+      },
+     });
+
+  } catch (err) {
+    return res.status(500).json({ error: err.message }).send("Server error");
+  }
 };
 
 module.exports = postRegister;
