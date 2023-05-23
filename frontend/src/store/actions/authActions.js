18a6375462e541bb5d5639731dc19fb95b36ed00
Asutosh <ashutoshguleria9@gmail.com>
2023-05-23 20:26:09 +0530

    prepare authentication actions

--- frontend/src/store/actions/authActions.js
+++ frontend/src/store/actions/authActions.js
@@ -1,1 +1,63 @@
-const authActions = {};
+import * as api from "../../api";
+
+export const authActions = {
+  // This is the redux action type that will be dispatched when the user details are set in the store.
+  // Defining the name of the action SET_USER_DETAILS here will help us to avoid typos in the reducer.
+  SET_USER_DETAILS: "AUTH.SET_USER_DETAILS",
+};
+
+export const getActions = (dispatch) => {
+  // getActions function returns an object with all the actions that can be dispatched.
+  return {
+    // login action is dispatched when the user logs in.
+    // If succesfull login or registration, we will use the history object from the react-router-dom
+    // which will execute push method to redirect user to the dashboard page, i.e.
+    // after successfully storing our JWT token in the redux store and our localStorage of browser.
+    login: (userDetails, history) => dispatch(login(userDetails, history)),
+    register: (userDetails, history) =>
+      dispatch(register(userDetails, history)),
+  };
+};
+
+const setUserDetails = (userDetails) => {
+  return {
+    type: authActions.SET_USER_DETAILS, // This is the action type that will be dispatched.
+    userDetails,
+  };
+};
+
+// Creating Actions
+// Redux thunk is used to make async calls in our Redux actions.
+const login = (userDetails, history) => async (dispatch) => {
+  const response = await api.login(userDetails);
+
+  if (response.error) {
+    // Show error message in alert
+  } else {
+    // userDetails is the user object returned from the API (server) after successful login.
+    // returns undefined if response.data is undefined else returns response.data.userDetails
+    const { userDetails } = response?.data;
+    localStorage.setItem("userDetails", JSON.stringify(userDetails));
+
+    // Dispatch the action to set the user details in the store.
+    dispatch(setUserDetails(userDetails));
+    history.push("/dashboard");
+  }
+};
+
+const register = (userDetails, history) => async (dispatch) => {
+  const response = await api.register(userDetails);
+
+  if (response.error) {
+    // Show error message in alert
+  } else {
+    // userDetails is the user object returned from the API (server) after successful login.
+    // returns undefined if response.data is undefined else returns response.data.userDetails
+    const { userDetails } = response?.data;
+    localStorage.setItem("userDetails", JSON.stringify(userDetails));
+
+    // Dispatch the action to set the user details in the store.
+    dispatch(setUserDetails(userDetails));
+    history.push("/dashboard");
+  }
+};
--- frontend/src/store/reducers/authReducer.js
+++ frontend/src/store/reducers/authReducer.js
@@ -1,25 +1,32 @@
-// This line initializes the initial state of the authentication section of your Redux store. 
+import { authActions } from "../actions/authActions";
+// This line initializes the initial state of the authentication section of your Redux store.
 // The 'userDetails' property is set to null, indicating no user is logged in when the app first starts.
 const initState = {
   userDetails: null,
-}
+};
 
-// This is the reducer function for the authentication section of your Redux store. 
+// This is the reducer function for the authentication section of our Redux store.
 // A reducer function is responsible for updating the state based on the actions it receives.
-// The reducer function takes the current state and an action as arguments. 
+// The reducer function takes the current state and an action as arguments.
 // If the state argument is not provided, it defaults to 'initState'.
 const reducer = (state = initState, action) => {
-  // The 'switch' statement is used to perform different actions based on different conditions. 
+  // The 'switch' statement is used to perform different actions based on different conditions.
   // In this case, it's looking at the 'type' property of the 'action' object.
   switch (action.type) {
-    // In the case where 'action.type' equals 'DUMMY_ACTION', the state is returned as is 
-    // (meaning no changes are made). The '...' is the spread operator, 
+    // In the case where 'action.type' equals 'DUMMY_ACTION', the state is returned as is
+    // (meaning no changes are made). The '...' is the spread operator,
     // which is used to make a copy of the state object.
-    case 'DUMMY_ACTION':
+    // case 'DUMMY_ACTION':
+    //   return {
+    //     ...state,
+    //   };
+
+    case authActions.SET_USER_DETAILS:
       return {
         ...state,
+        userDetails: action.userDetails,
       };
-      // The 'default' keyword specifies the code to run if there is no case match. 
+      // The 'default' keyword specifies the code to run if there is no case match.
       // In this instance, it returns the current state without making any changes.
       default:
         return state;
